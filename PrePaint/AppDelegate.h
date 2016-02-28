73844af16749be13013d47d37dfbd6f895cb7791
tringappsXiZhang <xizhang@tringapps.com>
2016-02-29 00:12:30 +0800

    Initial Commit

---
+++ PrePaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrePaint
+//
+//  Created by Xi Zhang on 2/29/16.
+//  Copyright © 2016 JingTang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
