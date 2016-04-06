1df2ac04453a1763f95f9abbaf8f6ed37c3ace11
tringappsXiZhang <xizhang@tringapps.com>
2016-04-06 01:17:20 -0700

    1.svg added

--- PrePaint/ToolTutorialViewController.h
+++ PrePaint/ToolTutorialViewController.h
@@ -15,9 +15,18 @@
 @property (strong, nonatomic) IBOutlet UIImageView *pencilImageView;
 @property (strong, nonatomic) IBOutlet UIImageView *veinBrushImageView;
 @property (strong, nonatomic) IBOutlet UIView *stepBarView;
+@property (strong, nonatomic) IBOutlet UIButton *previousButton;
+@property (strong, nonatomic) IBOutlet UIButton *nextButton;
+@property (strong, nonatomic) IBOutlet UILabel *stepNumberLabel;
+@property (strong, nonatomic) IBOutlet UIView *tutorialBgView;
+@property (strong, nonatomic) IBOutlet UIImageView *tutorialImageView;
 
 
 
 - (IBAction)backButtonAction:(id)sender;
+- (IBAction)previousButtonAction:(id)sender;
+- (IBAction)nextButtonAction:(id)sender;
+- (IBAction)gotitButtonAction:(UIButton*)sender;
+
 
 @end
--- PrePaint/ToolTutorialViewController.m
+++ PrePaint/ToolTutorialViewController.m
@@ -7,25 +7,56 @@
 //
 
 #import "ToolTutorialViewController.h"
+#import "PocketSVG.h"
 
 @interface ToolTutorialViewController (){
     CGRect _initialFrameOfObject;
     UIImageView *_tempImageView;
     BOOL _isXuanPaperOnScreen;
 }
+@property (strong, nonatomic)UIImageView *fullPaperImageView;
 
 @end
 
 @implementation ToolTutorialViewController
+@synthesize fullPaperImageView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if ([self isNeedToShowTutorial]) {
+        [self.tutorialBgView setHidden:NO];
+    }
+    else{
+        [self.tutorialBgView setHidden:YES];
+    }
+    
     [self resetFlags];
     [self.stepView.layer setCornerRadius:self.stepView.frame.size.height/2.0];
     [self addGestureRecognizersForImageView:self.xuanPaperImageView];
     [self addGestureRecognizersForImageView:self.pencilImageView];
     [self addGestureRecognizersForImageView:self.veinBrushImageView];
     // Do any additional setup after loading the view from its nib.
+}
+
+
+
+-(IBAction)gotitButtonAction:(UIButton*)sender
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:@(NO) forKey:@"isNeedToShowTutorial"];
+    [defaults synchronize];
+    [self.tutorialBgView setHidden:YES];
+    [self.tutorialBgView removeFromSuperview];
+}
+
+-(BOOL)isNeedToShowTutorial
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSNumber *isNeeded =  [defaults objectForKey:@"isNeedToShowTutorial"];
+    if (!isNeeded) {
+        return YES;
+    }
+    return [isNeeded boolValue];
 }
 
 -(void)resetFlags
@@ -97,6 +128,7 @@
                     } completion:^(BOOL finished) {
                         [gesture.view removeGestureRecognizer:[gesture.view.gestureRecognizers firstObject]];
                         [gesture.view setUserInteractionEnabled:NO];
+                        self.fullPaperImageView = self.xuanPaperImageView;
                         self.xuanPaperImageView = _tempImageView;
                         _tempImageView = nil;
                         
@@ -106,19 +138,23 @@
             }
             
             else if(gesture.view == self.pencilImageView){
+                
+               
                 [UIView animateWithDuration:0.5 animations:^{
-                    [gesture.view setAlpha:0.0];
+                    [gesture.view setAlpha:1.0];
                     
                 } completion:^(BOOL finished) {
+                    [self drawPencilPath];
                     self.pencilImageView = _tempImageView;
                     _tempImageView = nil;
                 }];
             }
             else{
                 [UIView animateWithDuration:0.5 animations:^{
-                    [gesture.view setAlpha:0.0];
+                    [gesture.view setAlpha:1.0];
                     
                 } completion:^(BOOL finished) {
+                    [self drawVeinBrushPath];
                     self.veinBrushImageView = _tempImageView;
                     _tempImageView = nil;
                 }];
@@ -155,11 +191,142 @@
     return YES;
 }
 
+-(void)drawPencilPath
+{
+    CGPathRef myPath = [PocketSVG pathFromSVGFileNamed:@"bigFlower"];
+   // myPath = [self CGPath_NGCreateCopyByScalingPathAroundCentre:myPath scale:0.5];
+    CAShapeLayer *myShapeLayer = [CAShapeLayer layer];
+    myShapeLayer.path = myPath;
+
+    myShapeLayer.strokeColor = [[UIColor darkGrayColor] CGColor];
+    myShapeLayer.lineWidth = 1.0;
+    myShapeLayer.lineCap = kCALineCapRound;
+    myShapeLayer.fillColor = [[UIColor clearColor] CGColor];
+    
+    CGRect rect = self.fullPaperImageView.layer.bounds;
+    CGFloat leftRightPadding = 140.0;
+    CGFloat topBottomPadding = 70.0;
+    rect.origin.x += leftRightPadding;
+    rect.origin.y += topBottomPadding;
+    rect.size.width = 620;
+    rect.size.height = 538-topBottomPadding;
+    [myShapeLayer setFrame:rect];
+    [self.fullPaperImageView.layer addSublayer:myShapeLayer];
+    CABasicAnimation *pathAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    pathAnimation.duration = 10.0;
+    pathAnimation.fromValue = [NSNumber numberWithFloat:0.0f];;
+    pathAnimation.toValue = [NSNumber numberWithFloat:1.0f];
+    
+    CABasicAnimation *colorAnimation = [CABasicAnimation animationWithKeyPath:@"fillColor"];
+    colorAnimation.duration = 10.0;
+    colorAnimation.fromValue = (id)[UIColor clearColor].CGColor;
+    colorAnimation.toValue = (id)[UIColor blackColor].CGColor;
+    [colorAnimation setFillMode:kCAFillModeBoth];
+    
+   
+    
+    CGAffineTransform translation = CGAffineTransformMakeTranslation(265,
+                                                                     204);
+    CGPathRef movedPath = CGPathCreateCopyByTransformingPath(myShapeLayer.path,
+                                                             &translation);
+
+    CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    anim.path = movedPath;
+    anim.calculationMode = kCAAnimationPaced;//kCAAnimationPaced;
+    //[anim setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
+    anim.duration = 10.0;
+    anim.delegate = self;
+    anim.fillMode = kCAFillModeForwards;
+    anim.removedOnCompletion = NO;
+    [myShapeLayer addAnimation:pathAnimation forKey:@"strokeAnime"];
+  //  [myShapeLayer addAnimation:colorAnimation forKey:@"colorAnime"];
+    [self.pencilImageView.layer addAnimation:anim forKey:@"draw"];
+}
+
+
+-(void)drawVeinBrushPath
+{
+    CGPathRef myPath = [PocketSVG pathFromSVGFileNamed:@"bigFlower"];
+    // myPath = [self CGPath_NGCreateCopyByScalingPathAroundCentre:myPath scale:0.5];
+    CAShapeLayer *myShapeLayer = [CAShapeLayer layer];
+    myShapeLayer.path = myPath;
+    
+    myShapeLayer.strokeColor = [[UIColor blackColor] CGColor];
+    myShapeLayer.lineWidth = 5.0;
+    myShapeLayer.lineCap = kCALineCapRound;
+    myShapeLayer.fillColor = [[UIColor clearColor] CGColor];
+    
+    CGRect rect = self.fullPaperImageView.layer.bounds;
+    CGFloat leftRightPadding = 140.0;
+    CGFloat topBottomPadding = 70.0;
+    rect.origin.x += leftRightPadding;
+    rect.origin.y += topBottomPadding;
+    rect.size.width = 620;
+    rect.size.height = 538-topBottomPadding;
+    [myShapeLayer setFrame:rect];
+    [self.fullPaperImageView.layer addSublayer:myShapeLayer];
+    CABasicAnimation *pathAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    pathAnimation.duration = 15.0;
+    pathAnimation.fromValue = [NSNumber numberWithFloat:0.0f];;
+    pathAnimation.toValue = [NSNumber numberWithFloat:1.0f];
+    
+    CABasicAnimation *colorAnimation = [CABasicAnimation animationWithKeyPath:@"fillColor"];
+    colorAnimation.duration = 15.0;
+    colorAnimation.fromValue = (id)[UIColor clearColor].CGColor;
+    colorAnimation.toValue = (id)[UIColor blackColor].CGColor;
+    [colorAnimation setFillMode:kCAFillModeBoth];
+    
+    
+    
+    CGAffineTransform translation = CGAffineTransformMakeTranslation(265,
+                                                                     204);
+    CGPathRef movedPath = CGPathCreateCopyByTransformingPath(myShapeLayer.path,
+                                                             &translation);
+    
+    CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    anim.path = movedPath;
+    anim.calculationMode = kCAAnimationPaced;//kCAAnimationPaced;
+    //[anim setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
+    anim.duration = 15.0;
+    anim.delegate = self;
+    anim.fillMode = kCAFillModeForwards;
+    anim.removedOnCompletion = NO;
+    [myShapeLayer addAnimation:pathAnimation forKey:@"strokeAnime"];
+    //  [myShapeLayer addAnimation:colorAnimation forKey:@"colorAnime"];
+    [self.veinBrushImageView.layer addAnimation:anim forKey:@"draw"];
+}
+
+
+
+
+-(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+    if (flag) {
+        
+    }
+}
 
 -(void)viewDidLayoutSubviews
 {
 //    [self.view setTranslatesAutoresizingMaskIntoConstraints:NO];
 //    NSLog(@"sdf");
+}
+
+
+-(CGPathRef)CGPath_NGCreateCopyByScalingPathAroundCentre:(CGPathRef)path scale:(float) scale
+{
+    CGRect bounding = CGPathGetPathBoundingBox(path);
+    CGPoint pathCenterPoint = CGPointMake(CGRectGetMidX(bounding), CGRectGetMidY(bounding));
+    
+    CGAffineTransform translateAndScale = CGAffineTransformTranslate( CGAffineTransformMakeScale(scale, scale), - pathCenterPoint.x, -pathCenterPoint.y) ;
+    CGAffineTransform translateBack = CGAffineTransformMakeTranslation(pathCenterPoint.x, pathCenterPoint.y);
+    
+    CGPathRef centeredAndScaled = CGPathCreateCopyByTransformingPath(path, &translateAndScale);
+    CGPathRef translatedPathRef = CGPathCreateCopyByTransformingPath(centeredAndScaled, &translateBack);
+    
+    CGPathRelease(centeredAndScaled);
+    
+    return translatedPathRef;
 }
 
 
@@ -192,5 +359,25 @@
                         [self.navigationController popViewControllerAnimated:NO];
                     }
                     completion:nil];
+}
+
+-(void)changeStepText
+{
+
+}
+
+- (IBAction)previousButtonAction:(id)sender {
+    NSInteger step = [self.stepNumberLabel.text integerValue];
+    if (step > 1) {
+         [self.stepNumberLabel setText:[NSString stringWithFormat:@"%d",step-1]];
+    }
+   
+}
+
+- (IBAction)nextButtonAction:(id)sender {
+    NSInteger step = [self.stepNumberLabel.text integerValue];
+//    if (step > 1) {
+        [self.stepNumberLabel setText:[NSString stringWithFormat:@"%d",step+1]];
+//    }
 }
 @end
